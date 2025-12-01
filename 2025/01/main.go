2537c69e2799a327a71d3da9d087b26db06f4b13
Felipe Stillner Eufranio <felipeeufranio@MacBook-Air-de-Felipe.local>
2025-12-01 11:28:42 +0100

    Add solution for day 1 2025

--- 2025/01/main.go
+++ 2025/01/main.go
@@ -3,21 +3,75 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"math"
+	"strconv"
+	"strings"
 	"time"
 )
 
 //go:embed input.txt
 var input string
 
-func Parse(input string) {
+func Parse(input string) []int {
+	var numbers []int
+	input = strings.TrimSpace(input)
+	var lines = strings.Split(input, "\n")
+	for _, line := range lines {
+		var n, _ = strconv.Atoi(line[1:])
+		if line[0] == 'L' {
+			numbers = append(numbers, -n)
+		} else {
+			numbers = append(numbers, n)
+		}
+	}
+	return numbers
 }
 
 func Part1(input string) int {
-	return 0
+	numbers := Parse(input)
+	now := 50
+	n_zeros := 0
+
+	for _, n := range numbers {
+		now += n
+
+		now = now % 100
+
+		if now == 0 {
+			n_zeros++
+		}
+	}
+
+	return n_zeros
 }
 
 func Part2(input string) int {
-	return 0
+	numbers := Parse(input)
+	dial := 50
+	n_zeros := 0
+
+	for _, n := range numbers {
+		n_zeros += int(math.Abs(float64(n / 100)))
+
+		dial_ant := dial
+		dial += n % 100
+
+		if (dial_ant > 0 && dial < 0) || (dial_ant < 0 && dial > 0) || dial == 0 {
+			n_zeros++
+		}
+
+		if dial >= 100 {
+			dial -= 100
+			n_zeros++
+		}
+
+		if dial <= -100 {
+			dial += 100
+			n_zeros++
+		}
+	}
+
+	return n_zeros
 }
 
 func main() {
--- 2025/01/main_test.go
+++ 2025/01/main_test.go
@@ -10,7 +10,7 @@ var inputTest string
 
 func TestPart1(t *testing.T) {
 	result := Part1(inputTest)
-	expected := 0
+	expected := 3
 	if result != expected {
 		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
 	}
@@ -18,7 +18,7 @@ func TestPart1(t *testing.T) {
 
 func TestPart2(t *testing.T) {
 	result := Part2(inputTest)
-	expected := 0
+	expected := 6
 	if result != expected {
 		t.Errorf("Result is incorrect, got: %d, want: %d.", result, expected)
 	}
